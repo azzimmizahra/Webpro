a4165d62440ee689012ac6fdec12e305b7d49298
azzimmizahra <zhaifa520@gmail.com>
2026-04-14 16:51:10 +0700

    Delete Praktikum 6 directory

--- Praktikum 6/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-
-Route::get('/', [HomeController::class, 'home']);
-Route::get('/about', [HomeController::class, 'about']);
-Route::get('/profile', [HomeController::class, 'profile']);
